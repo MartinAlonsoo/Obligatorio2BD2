300c322822bfb5f2e12484aaaea649df86661f7d
MartinAlonso <MA291799@fi365.ort.edu.uy>
2025-06-16 18:17:30 -0300

    Parte 4.2 hecha

---
+++ SCRIPTS_NUEVOS/Parte4/Parte4.2.js
@@ -0,0 +1,27 @@
+db.personajes_progreso.aggregate([
+  // Etapa 1: Filtra personajes con progreso <= 60%
+  {
+    $match: {
+      "estadisticas.progreso_general": { $lte: 60 } // $lte es "less than or equal"
+    }
+  },
+  // Etapa 2: Ordena por enemigos derrotados de mayor a menor
+  {
+    $sort: {
+      "estadisticas.enemigos_derrotados": -1 // -1 para orden descendente
+    }
+  },
+  // Etapa 3: Limita el resultado a los 5 primeros
+  {
+    $limit: 5
+  },
+  // Etapa 4: Formatea la salida para que sea clara
+  {
+    $project: {
+      _id: 0,
+      email_jugador: "$_id",
+      enemigos_derrotados: "$estadisticas.enemigos_derrotados",
+      progreso: "$estadisticas.progreso_general"
+    }
+  }
+])
